105bac3361a8b5b03cd397a74bdedf04f99012a3
renata242 <59351081+renata242@users.noreply.github.com>
2022-03-25 05:51:23 +0600

    Delete store/main directory

--- store/main/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class User(models.Model):
-
-    email = models.EmailField('email', max_length=50)
-    pwd = models.CharField('password', max_length=50)
-
-    def __str__(self):
-        return self.title
