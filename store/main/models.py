c3081006164cbaee08faf1cd398a6c31b75f3563
renata242 <59351081+renata242@users.noreply.github.com>
2022-04-15 11:17:01 +0600

    Add files via upload

--- store/main/models.py
+++ store/main/models.py
@@ -1,9 +1,30 @@
-from django.db import models
+from django.db import migrations, models
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import AbstractUser
 
-class User(models.Model):
+class Medicaments(models.Model):
 
-    email = models.EmailField('email', max_length=50)
-    pwd = models.CharField('password', max_length=50)
+    # field to store article category
+    meds_category = models.CharField(max_length=200)
 
-    def __str__(self):
-        return self.title
+    # field to store artcile name
+    meds_name = models.CharField(max_length=200)
+
+    # field to store article's text
+    meds_description = models.TextField()
+
+    # field to store article's date
+    meds_packing_date = models.DateTimeField(auto_now_add=True)
+
+    # article rating field
+    meds_rating = models.FloatField(default=0.0)
+
+    # field to store link to editor (foreign key, M to 1 relationship)
+    meds_manufacturer = models.CharField(max_length=200)
+
+    # magic method to retrieve str value of article name and
+    # return it when the method is called
+    def __str__(self) -> str:
+        return self.meds_name
+
