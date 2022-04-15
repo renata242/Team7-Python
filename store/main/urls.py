b6dfa6d2f57a13a73f66787f20fd7e9b89cf3147
renata242 <59351081+renata242@users.noreply.github.com>
2022-04-15 11:12:53 +0600

    Add files via upload

--- store/main/urls.py
+++ store/main/urls.py
@@ -1,13 +1,15 @@
+from django.contrib import admin
 from django.urls import path
 from . import views
 
 
 urlpatterns = [
-    path('', views.index),
-    path('about', views.about),
-    path('login', views.login),
-    path('shop', views.shop),
-    path('contactus', views.contactus),
-    path('liked', views.liked),
-    path('basket', views.basket),
+    path('', views.index, name='index'),
+    path('about', views.about, name='about'),
+    path('login', views.login, name='login'),
+    path('signup', views.signup, name='signup'),
+    path('shop', views.shop, name='shop'),
+    path('contactus', views.contactus, name='contactus'),
+    path('liked', views.liked, name='liked'),
+    path('basket', views.basket, name='basket'),
 ]
