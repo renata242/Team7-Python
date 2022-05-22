7c293a58964b74a951addccc0a892269557924f4
renata242 <59351081+renata242@users.noreply.github.com>
2022-05-22 14:10:25 +0600

    Add files via upload

--- chat/models.py
+++ chat/models.py
@@ -8,3 +8,4 @@ class Message(models.Model):
     value = models.CharField(max_length=10000000)
     date = models.DateTimeField(default=datetime.now, blank=True)
     room = models.CharField(max_length=1000000)
+    user = models.CharField(max_length=1000000)
--- chat/views.py
+++ chat/views.py
@@ -7,29 +7,31 @@ def home(request):
 
 
 def room(request, room):
-
+    username = request.GET.get('username')
     room_details = Room.objects.get(name=room)
     return render(request, 'room.html', {
+        'username': username,
         'room': room,
         'room_details': room_details,
     })
 
 def checkview(request):
     room = request.POST['room_name']
+    username = request.POST['username']
 
     if Room.objects.filter(name=room).exists():
-        return redirect('/'+room)
+        return redirect('/'+room+'/?username='+username)
     else:
         new_room = Room.objects.create(name=room)
         new_room.save()
-        return redirect('/'+room)
+        return redirect('/'+room+'/?username='+username)
 
 def send(request):
     message = request.POST['message']
-
+    username = request.POST['username']
     room_id = request.POST['room_id']
 
-    new_message = Message.objects.create(value=message, room=room_id)
+    new_message = Message.objects.create(value=message, user=username, room=room_id)
     new_message.save()
 
 def getMessages(request,  room):
