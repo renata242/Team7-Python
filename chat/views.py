6ebc04c1a1041cdf03637fb5f6b405760400b40a
renata242 <59351081+renata242@users.noreply.github.com>
2022-05-23 14:22:57 +0600

    Add files via upload

--- chat/views.py
+++ chat/views.py
@@ -7,24 +7,26 @@ def home(request):
 
 
 def room(request, room):
-    username = request.GET.get('username')
+    username = request.GET.get('username') # henry
     room_details = Room.objects.get(name=room)
     return render(request, 'room.html', {
+
         'username': username,
         'room': room,
         'room_details': room_details,
     })
+
 
 def checkview(request):
     room = request.POST['room_name']
     username = request.POST['username']
 
     if Room.objects.filter(name=room).exists():
-        return redirect('/'+room+'/?username='+username)
+        return redirect('/chat/'+room+'/?username='+username)
     else:
         new_room = Room.objects.create(name=room)
         new_room.save()
-        return redirect('/'+room+'/?username='+username)
+        return redirect('/chat/'+room+'/?username='+username)
 
 def send(request):
     message = request.POST['message']
